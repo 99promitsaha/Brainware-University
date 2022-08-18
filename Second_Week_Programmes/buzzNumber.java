323e623601e85c4099d2a59abb451ad4e266bb55
Promit Saha <promitsaha154@gmail.com>
2022-08-18 20:53:06 +0530

    Second Week Programmes

---
+++ Second_Week_Programmes/buzzNumber.java
@@ -0,0 +1,28 @@
+package Second_Week_Programmes;
+import java.util.Scanner;
+
+public class buzzNumber {
+    public static int check (int a){
+        if (a%10==7 || a%7==0){
+            return (1);
+        }
+        else {
+            return (0);
+        }
+    }
+    public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check Buzz Number or Not!");
+
+        int number = sc.nextInt();
+
+        int check = check(number);
+
+        if (check == 1){
+            System.out.println(number+ " is a Buzz Number");
+        }
+        else {
+            System.out.println(number + " is not a Buzz Number");
+        }
+    }
+}
