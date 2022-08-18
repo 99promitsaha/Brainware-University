2061e1f3d755686ea52c6b5676040eae13e7cac7
Promit Saha <promitsaha154@gmail.com>
2022-08-18 21:04:35 +0530

    Second Commit

--- Second_Week_Programmes/factorialOf12.java
+++ Second_Week_Programmes/factorialOf12.java
@@ -3,9 +3,23 @@ package Second_Week_Programmes;
 import java.util.Scanner;
 
 public class factorialOf12 {
+    public static int calculate(int a){
+        int num = a, result = 1, i;
+
+        for (i=1; i<=num; i++){
+            result=result*i;
+        }
+
+        return(result);
+    }
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to calculate Factorial: ");
 
-        
+        int number = sc.nextInt();
+        int calc = calculate(number);
+
+        System.out.println("The value of factorial of "+ number+ " is "+ calc);
+
     }
 }
