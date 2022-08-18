a7dc3d74734fb6fd140759d80483cf2a2d5247e5
Promit Saha <promitsaha154@gmail.com>
2022-08-18 20:55:57 +0530

    Promit Saha folder names modified

---
+++ Second_Week_Programmes/factorialOf12.java
@@ -0,0 +1,11 @@
+package Second_Week_Programmes;
+
+import java.util.Scanner;
+
+public class factorialOf12 {
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        
+    }
+}
