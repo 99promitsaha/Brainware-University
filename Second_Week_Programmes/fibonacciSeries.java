db134133ab23ebbe5c6dfe7d7c7e5a46bede8c4c
Promit Saha <promitsaha154@gmail.com>
2022-08-19 09:47:37 +0530

    19/08/2022 @promitsaha

---
+++ Second_Week_Programmes/fibonacciSeries.java
@@ -0,0 +1,29 @@
+package Second_Week_Programmes;
+
+import java.util.Scanner;
+
+public class fibonacciSeries {
+    public static void print(int a){
+        int num1=0, num2=1;
+        int counter = 0;
+
+        while (counter<a){
+            System.out.print(num1+ " ");
+
+            //Swap
+            int num3 = num1+num2;
+            num1=num2;
+            num2=num3;
+            counter = counter+1;
+        }
+
+    }
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the number of Fibonacci Numbers ");
+        int num = sc.nextInt();
+
+        print(num);
+    }
+}
